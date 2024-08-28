aa720945666aeb7d3dd92b5be651b02fcb776c40
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-08-28 20:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode, DataReturnMode
 import plotly.graph_objs as go
+st.cache.clear()
 
 # Load Data for Top 100 Nifty Stocks
 @st.cache_data
